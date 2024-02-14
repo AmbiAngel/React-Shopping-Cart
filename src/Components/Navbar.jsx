90cefb98f1cdb21b492e7645decda111a899ac92
Angel <angelambisilva@gmail.com>
2024-02-13 21:33:37 -0500

    Style bg colors to look nicer

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,9 +7,10 @@ export default function NavBar({numOfItems}) {
   return (
     <div id="nav-bar">
         <ul>
-            <li><Link to={'home'}>Home</Link></li>
-            <li><Link to={'shopping'}>Store</Link></li>
-            <li className='cart'><Link to={'cart'}>Cart: <span className='num-of-items'>{numOfItems}</span></Link></li>
+            <li><Link className='nav-text' to={'home'}>Home</Link></li>
+            <li><Link className='nav-text' to={'shopping'}>Store</Link></li>
+            
+            <li className='cart'><Link className='nav-text' to={'cart'}>Cart: <span className='num-of-items'>{numOfItems}</span></Link></li>
         </ul>
     </div>
   );
