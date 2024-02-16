b492a47ae971ae6080ad8bc9d052eb549e4f8664
Angel <angelambisilva@gmail.com>
2024-02-16 01:22:21 -0500

    Correct missing css class on about page

--- src/Components/About-Page.jsx
+++ src/Components/About-Page.jsx
@@ -2,7 +2,7 @@ import './About-Page.css'
 
 export default function AboutPage(){
     return(
-        <div className="about-container">
+        <div className="about-container main-content">
             <h2>This is a fictional store and none of the products displayed here exist.</h2>
             <br />
             <h2>Products' information and images: Fake Store API.</h2>
